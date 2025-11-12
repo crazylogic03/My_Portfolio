af9a48e722b9973b109bf95658433636836b66aa
crazylogic03 <saithrishul2006@gmail.com>
2025-11-12 23:24:16 +0530

    Updated Project

--- script.js
+++ script.js
@@ -81,12 +81,12 @@ const projects = [
     },
     {
         id: 7,
-        title: "ScreenTime Recorder",
-        description: "An open-source tool designed to help users track and analyze their computer usage. It features real-time activity monitoring, automatic logging, and visual dashboards to provide insights into productivity and digital habits. Built using React.js, NodeJs, MongoDB with a focus on clean UI, efficient data handling, and usability. Contributed About 10+ features and improvements through active development and testing.",
-        image: "str.png",
-        techStack: ["React", "Node", "JavaScript", "MongoDB"],
-        liveUrl: "https://screentime-recoder.vercel.app/",
-        githubUrl: "https://github.com/crazylogic03/Screentime-recoder",
+        title: "EmptyBagss [UpComing]",
+        description: "An Travel AI Which Design the Trip Based on Number of Days , And Budget With many Other Features",
+        // image: "str.png",
+        techStack: ["React", "NodeJS", "JavaScript", "MongoDB"],
+        // liveUrl: "https://screentime-recoder.vercel.app/",
+        githubUrl: "https://github.com/crazylogic03/EmptyBagss",
     }
 ];
 
